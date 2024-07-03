7b5b629964468b246552d1c9ae217791cf16c040
Buhay Malaya <malayabuhay@gmail.com>
2024-07-03 19:08:43 -0400

    test3 not working

--- day1.py
+++ day1.py
@@ -13,15 +13,6 @@
 # To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
 # and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
-# FROM MY UNDERSTANDING:
-
-# THE PARAMETERS INCLUDE - 
-# - TWO ARRAYS BOTH CONTAINING INTEGERS ONLY (CAN BE ASSORTED IN ANY WAY EXCEPT ASCENDING)
-# - TWO INTEGERS (m, n) REPRESENTING AMOUNT OF ELEMENTS IN NUMS1, NUMS2
-# OUTPUT SHOULD NOT BE RETURNED, INSTEAD NUMS1 THAT CONTAINS NUMS2, ASC
-# DO NOT INCLUDE 0 IN THE MERGED OUTPUT
-
-
 
 # Example 1:
 
@@ -59,9 +50,40 @@
 
 # Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 
+# FROM MY UNDERSTANDING:
+
+# THE PARAMETERS INCLUDE - 
+# - TWO ARRAYS BOTH CONTAINING INTEGERS ONLY (CAN BE ASSORTED IN ANY WAY EXCEPT ASCENDING)
+# - TWO INTEGERS (m, n) REPRESENTING AMOUNT OF ELEMENTS IN NUMS1, NUMS2
+# OUTPUT SHOULD NOT BE RETURNED, INSTEAD NUMS1 THAT CONTAINS NUMS2, ASC
+# DO NOT INCLUDE 0 IN THE MERGED OUTPUT
+
+# create pointers 
+# p1 for nums1, since indexes start at 0, pointer1 should be m-1
+# same for nums2, pointer2 = n-1
+# pointer for nums1 as merged arr should be (m+n)-1 because it has to be big enough to accommodate non 0 elements in both arr
+
+# first define the pointers after function
+# while loop to iterate through both arr as long as pointers are >= 0
+# compare values of both pointers 
+# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
+        pointer1 = m - 1
+        pointer2 = n - 1
+        pointer_output = (m+n) - 1
+
+        while pointer1 >= 0 and pointer2 >= 0:
+            if nums1[pointer1] > nums2[pointer2]: # if the last value of nums1 is bigger than that of nums2
+                nums1[pointer_output] = nums1[pointer1] # assign the last value for nums1 output with nums1[pointer1]
+                pointer1 -= 1 # iterate down the arr since the loop is starting at the very end or last index until 0 is reached (see constraint of while loop)
+            else:
+                nums1[pointer_output] = nums2[pointer2] # unless the last value of nums2 is > than that of nums1
+                pointer2 -= 1 # iterate down the array until 0 is reached
+                
+            pointer_output -=1
+
+
+
         
