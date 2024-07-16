c8595a8535ab259d3152df50b8e2c1ceb4493c51
Buhay Malaya <malayabuhay@gmail.com>
2024-07-16 11:39:41 -0400

    1

--- day3.py
+++ day3.py
@@ -4,11 +4,14 @@
 # Companies
 # Hint
 
-# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+# Given an integer array nums sorted in non-decreasing order,
+# remove the duplicates in-place such that each unique element appears only once. 
+# The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
 # Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
 
-#     Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+#     Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. 
+#  The remaining elements of nums are not important as well as the size of nums.
 #     Return k.
 
 # Custom Judge:
@@ -51,3 +54,5 @@
 #     -100 <= nums[i] <= 100
 #     nums is sorted in non-decreasing order.
 
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
