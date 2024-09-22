09009b61062f6908cec6d09c035533333af6bafd
Buhay Malaya <malayabuhay@gmail.com>
2024-09-22 08:38:40 -0700

    constraints

--- day4.py
+++ day4.py
@@ -35,3 +35,9 @@
 # Output: 7, nums = [0,0,1,1,2,3,3,_,_]
 # Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
 # It does not matter what you leave beyond the returned k (hence they are underscores).
+
+# Constraints:
+
+#     1 <= nums.length <= 3 * 104
+#     -104 <= nums[i] <= 104
+#     nums is sorted in non-decreasing order.
