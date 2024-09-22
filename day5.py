42a263706f5028601c970d9dd471f4307f8dd99e
Buhay Malaya <malayabuhay@gmail.com>
2024-09-22 08:45:47 -0700

    soln

--- day5.py
+++ day5.py
@@ -25,6 +25,6 @@
 #     1 <= prices.length <= 105
 #     0 <= prices[i] <= 104
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
         
