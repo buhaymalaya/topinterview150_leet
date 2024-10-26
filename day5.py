528844b63163d61f90ad2a2a82936efa2a07d8e8
Buhay Malaya <malayabuhay@gmail.com>
2024-10-25 19:03:37 -0700

    cl

--- day5.py
+++ day5.py
@@ -48,3 +48,6 @@ solution = Solution()
 # Test cases
 prices1 = [7, 1, 5, 3, 6, 4]
 prices2 = [7, 6, 4, 3, 1]
+
+print("Maximum profit for prices1:", solution.maxProfit(prices1))  # Expected output: 5
+print("Maximum profit for prices2:", solution.maxProfit(prices2))  # Expected output: 0
