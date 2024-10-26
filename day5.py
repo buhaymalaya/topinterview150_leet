821780a2765bcff3444e2da7bd7fea50f243fb2c
Buhay Malaya <malayabuhay@gmail.com>
2024-10-25 19:04:46 -0700

    tp

--- day5.py
+++ day5.py
@@ -51,3 +51,18 @@ prices2 = [7, 6, 4, 3, 1]
 
 print("Maximum profit for prices1:", solution.maxProfit(prices1))  # Expected output: 5
 print("Maximum profit for prices2:", solution.maxProfit(prices2))  # Expected output: 0
+
+
+# two pointer
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0  # Buy pointer
+        max_profit = 0
+        
+        # Iterate with the right pointer starting from the second day
+        for right in range(1, len(prices)):
+            # If current price is less than the price at the buy pointer, move left to right
+           
