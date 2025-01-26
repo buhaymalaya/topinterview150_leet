4b824b0a5ea59ad41e2e2ebeaec0cddc724ef947
Buhay Malaya <malayabuhay@gmail.com>
2025-01-25 17:15:12 -0800

    5

--- day20.py
+++ day20.py
@@ -49,3 +49,14 @@ print(length_of_last_word(s3))  # Output: 6
 def length_of_last_word(s: str) -> int:
     length = 0
     i = len(s) - 1
+
+    # Skip trailing spaces
+    while i >= 0 and s[i] == ' ':
+        i -= 1
+
+    # Count the characters of the last word
+    while i >= 0 and s[i] != ' ':
+        length += 1
+        i -= 1
+
+    return length
