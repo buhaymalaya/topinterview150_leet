efb22a6dd1c3c60089a66c30514653fbd8050ca6
Buhay Malaya <malayabuhay@gmail.com>
2025-02-22 23:32:04 -0800

    2

--- day 8.py
+++ day 8.py
@@ -27,3 +27,8 @@
 
 def majorityElement(nums):
     candidate, count = None, 0
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += 1 if num == candidate else -1
