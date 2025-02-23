f716f542d0af3d59a5c43b88dc87745125408fab
Buhay Malaya <malayabuhay@gmail.com>
2025-02-22 23:35:37 -0800

    5

--- day9.py
+++ day9.py
@@ -34,3 +34,29 @@
 #     Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
 #     Could you do it in-place with O(1) extra space?
 
+def rotate(nums, k):
+    n = len(nums)
+    k %= n  # for cases where k > n
+
+    # helper function to reverse a portion of the array
+    def reverse(start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+    # 1: Reverse the entire array
+    reverse(0, n - 1)
+    # 2: Reverse the first k elements
+    reverse(0, k - 1)
+    # 3: Reverse the remaining elements
+    reverse(k, n - 1)
+
+
+nums1 = [1,2,3,4,5,6,7]
+rotate(nums1, 3)
+print(nums1)  # [5,6,7,1,2,3,4]
+
+nums2 = [-1,-100,3,99]
+rotate(nums2, 2)
+print(nums2)  # [3,99,-1,-100]
