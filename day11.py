6e1f11da3240fe73aec14e339c869aaa7c3064e8
Buhay Malaya <malayabuhay@gmail.com>
2025-02-28 23:50:33 -0800

    2

--- day11.py
+++ day11.py
@@ -23,3 +23,15 @@
 #     1 <= nums.length <= 104
 #     0 <= nums[i] <= 105
 
+def canJump(nums):
+    farthest = 0
+    for i, jump in enumerate(nums):
+        if i > farthest:
+            return False
+        farthest = max(farthest, i + jump)
+        if farthest >= len(nums) - 1:
+            return True
+    return False
+
+print(canJump([2,3,1,1,4]))  # true
+print(canJump([3,2,1,0,4]))  # false
